8505ad5376be663446ff57d3db42aa4ff634f9de
Thomas Werner <35447824+huddeldaddel@users.noreply.github.com>
2019-09-30 19:40:09 +0200

    Changed mapping of homepage

--- src/main/kotlin/de/huddeldaddel/mjournal/controllers/HomeController.kt
+++ src/main/kotlin/de/huddeldaddel/mjournal/controllers/HomeController.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/")
 class HomeController {
 
-    @GetMapping(value = ["/home"])
+    @GetMapping(value = ["/"])
     fun getHome(): String = "home"
 
 }
